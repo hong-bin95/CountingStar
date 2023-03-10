db7981d0f4d23c34742c5d6a603bf945f5f4a650
홍빈 <105191633+hong-bin95@users.noreply.github.com>
2023-03-10 11:33:31 +0900

    🎉 [S08P22A201-14] BE / 프로젝트 생성

---
+++ backend/src/main/java/com/a201/countingstar/CountingStarApplication.java
@@ -0,0 +1,13 @@
+package com.a201.countingstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountingStarApplication.class, args);
+    }
+
+}
