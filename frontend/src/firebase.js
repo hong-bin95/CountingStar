32c6bbacfe59a92a20c841c606c0d52579ec23ce
Son Jeong Hun <thswjdgns9@naver.com>
2023-03-16 09:36:16 +0900

    📝[S08P22A201-74] FE/ 파이어베이스 기본 설정

---
+++ frontend/src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4WCdJnbnx-qFXuOnobgbibZhpjx-qAo0",
+  authDomain: "counting-star-e6435.firebaseapp.com",
+  projectId: "counting-star-e6435",
+  storageBucket: "counting-star-e6435.appspot.com",
+  messagingSenderId: "474995817444",
+  appId: "1:474995817444:web:01869a981ea646b23ba5f1"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
