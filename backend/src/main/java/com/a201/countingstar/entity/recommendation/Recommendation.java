d498165d839523632fdbbab2eb6261aac271c5cc
홍빈 <105191633+hong-bin95@users.noreply.github.com>
2023-03-16 14:15:26 +0900

    🚀 [S08P22A201-126] BE/DB 스키마 수정

--- backend/src/main/java/com/a201/countingstar/entity/recommendation/Recommendation.java
+++ backend/src/main/java/com/a201/countingstar/entity/recommendation/Recommendation.java
@@ -1,10 +1,18 @@
 package com.a201.countingstar.entity.recommendation;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-//@Entity
-//@Table(name="recommendation")
-//public class Recommendation {
-//
-//}
+@Entity
+@Table(name="recommendation")
+public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // sql에 시키기
+    @Column(name="recommendation_id")
+    private int crecommendationId;
+
+    private String title;
+    @Lob
+    private String contents;
+
+}
