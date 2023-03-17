2b8e1124961a60ddac29a7ea51d22f7d96804ee3
박찬호 <cksgh0109@naver.com>
2023-03-17 17:55:40 +0900

    🚀[S08P22A201-87] FE / 상세페이지 메인컴포넌트 생성

---
+++ frontend/src/components/DetailsMain.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import logo from '../logo.svg'
+
+function DetailsMain() {
+
+    const [name, setName] = useState('0');
+
+    return (
+        <div>
+  
+        </div>
+    );
+}
+
+export default DetailsMain;
--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DetailsMain from '../components/DetailsMain';
 
 function Detail() {
     return (
