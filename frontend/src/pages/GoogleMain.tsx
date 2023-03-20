4f51e2f1852f7fec46197ea149f5bda74270c10c
Son Jeong Hun <thswjdgns9@naver.com>
2023-03-20 13:38:18 +0900

    🚀  FE/ 이미지 파일 추가 및 일부 설정 추가

--- frontend/src/pages/GoogleMain.tsx
+++ frontend/src/pages/GoogleMain.tsx
@@ -2,20 +2,20 @@ import {GoogleMap, LoadScript, MarkerF} from '@react-google-maps/api'
 import {useMemo} from 'react'
 import styled from 'styled-components'
 
-function MapComponent() {
+function GoogleMain() {
   const center = useMemo(() => ({lat: 37.541, lng: 126.986}), [])
 
   return (
     <Wrapper>
-      <LoadScript googleMapsApiKey="AIzaSyCNuKH6J4n6OgN_d4Rzga8jujPXfx3yOWM">
-        <GoogleMap zoom={18} center={center} mapContainerClassName="map-container">
+      <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_MAP_KEY}`}>
+        <GoogleMap zoom={13} center={center} mapContainerClassName="map-container">
           <MarkerF position={center} icon={{url: '/images/icons/map_marker.svg', scale: 5}} />
         </GoogleMap>
       </LoadScript>
     </Wrapper>
   )
 }
-export default MapComponent
+export default GoogleMain
 
 const Wrapper = styled.div`
   .map-container {
