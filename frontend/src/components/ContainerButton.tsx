b9debe9aa80236e2dd4283127594d51c4de554e5
박찬호 <cksgh0109@naver.com>
2023-03-20 18:05:11 +0900

    🚀 [S08P22A201-137] FE / 상세페이지 메인 컴포넌트 버튼 이미지 변경기능

--- frontend/src/components/ContainerButton.tsx
+++ frontend/src/components/ContainerButton.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import styled from 'styled-components';
+import logo from '../logo.svg';
 
 function ContainerButton() {
+    
+    const [now, setNow] = useState<number>(1);
+    const moveUp = () => setNow(now - 1);
+    const moveDown = () => setNow(now + 1);
+
+    // const btnUp = useRef<HTMLImageElement>(null);
+    // const btnDown = useRef<HTMLImageElement>(null);
+    const btnUp = useRef() as React.MutableRefObject<HTMLImageElement>;
+    const btnDown = useRef() as React.MutableRefObject<HTMLImageElement>;
+
+    useEffect(() => {
+        if(now === 1) {
+            btnDown.current.style.display = "none";
+            btnUp.current.style.display = "inline";
+
+        }
+        else{
+            btnDown.current.style.display = "inline";
+            btnUp.current.style.display = "none";
+        }
+    }, [now]);
+
     return (
         <div>
-            123
+            <img src='../logo.svg' alt="up" onClick={moveUp} ref={btnUp}/>
+            <img src="./박찬호.jpg" alt="do" onClick={moveDown} ref={btnDown}/>
         </div>
     );
 }
