11292bd1dea5ad057b7ba7cf179056dd82398cf4
Son Jeong Hun <thswjdgns9@naver.com>
2023-03-20 12:52:24 +0900

    📝 FE/ 구글맵 기본 구현

--- frontend/src/pages/GoogleMain.tsx
+++ frontend/src/pages/GoogleMain.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import {GoogleMap, LoadScript, MarkerF} from '@react-google-maps/api'
+import {useMemo} from 'react'
+import styled from 'styled-components'
 
-function GoogleMain() {
-  return <div>Google Map</div>;
+function MapComponent() {
+  const center = useMemo(() => ({lat: 37.541, lng: 126.986}), [])
+
+  return (
+    <Wrapper>
+      <LoadScript googleMapsApiKey="AIzaSyCNuKH6J4n6OgN_d4Rzga8jujPXfx3yOWM">
+        <GoogleMap zoom={18} center={center} mapContainerClassName="map-container">
+          <MarkerF position={center} icon={{url: '/images/icons/map_marker.svg', scale: 5}} />
+        </GoogleMap>
+      </LoadScript>
+    </Wrapper>
+  )
 }
+export default MapComponent
 
-export default GoogleMain;
+const Wrapper = styled.div`
+  .map-container {
+    width: 100vw;
+    height: 100vh;
+  }
+`
