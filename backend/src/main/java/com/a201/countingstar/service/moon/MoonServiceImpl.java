acb7d115b51c09a102aba8f22270e38a2a97e2df
SJJ <jeenju1995@gmail.com>
2023-03-23 17:56:29 +0900

    💫 [S08P22A201-177] BE/달 api 호출하기 추가 및 수정

---
+++ backend/src/main/java/com/a201/countingstar/service/moon/MoonServiceImpl.java
@@ -0,0 +1,15 @@
+package com.a201.countingstar.service.moon;
+
+import com.a201.countingstar.dto.moon.MoonResponseDto;
+import com.a201.countingstar.moon.MoonEnum;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+//@AllArgsConstructor
+public class MoonServiceImpl implements MoonService {
+    @Override
+    public MoonResponseDto getMoonUrl(int value) {
+        return null;
+    }
+}
