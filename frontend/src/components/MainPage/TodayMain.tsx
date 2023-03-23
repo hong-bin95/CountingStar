090728fbcf624ccd276936a5256bea0faa6ad891
ljh991004 <ljh991004@naver.com>
2023-03-23 17:54:08 +0900

    💫 [S08P22A201-194] FE / 오늘 별점 axios 연결

--- frontend/src/components/MainPage/TodayMain.tsx
+++ frontend/src/components/MainPage/TodayMain.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Logo from "../Logo";
 import TodayBox from "../../components/MainPage/TodayBox";
+import axios from "axios";
 
 type Props = {};
 
+interface spot {
+  spotName: string;
+  grade: number;
+}
+
 function TodayMain({}: Props) {
+  const [spotList, setSpotList] = useState<Array<spot>>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://counting-star.com/api/spot/ranking", {
+        params: {
+          baseDateYear: "2023",
+          baseDateMonth: "03",
+          baseDateDay: "23",
+          baseDateHour: "11",
+          baseDateMinute: "00",
+          limit: 5,
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        console.log("구분");
+
+        setSpotList(response.data.data);
+        console.log(spotList);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <div className="grid grid-cols-12 gap-4 mb-7">
@@ -19,15 +52,11 @@ function TodayMain({}: Props) {
         <div className="col-span-2 ">버튼 컴포넌트</div>
       </div>
       <div className="grid grid-cols-12 gap-10 mx-auto my-1 ">
-        <div className="col-span-4">
-          <TodayBox />
-        </div>
-        <div className="col-span-4">
-          <TodayBox />
-        </div>
-        <div className="col-span-4">
-          <TodayBox />
-        </div>
+        {spotList.map((spot) => (
+          <div className="col-span-4">
+            <TodayBox spotName={spot.spotName} grade={spot.grade} />
+          </div>
+        ))}
       </div>
     </>
   );
