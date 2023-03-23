cd4dbff3eaaad275b83d53980a157560b198b984
홍빈 <105191633+hong-bin95@users.noreply.github.com>
2023-03-23 16:44:42 +0900

    🚀 [S08P22A201-173] BE 날씨 API 추가

---
+++ backend/src/main/java/com/a201/countingstar/service/weather/WeatherService.java
@@ -0,0 +1,14 @@
+package com.a201.countingstar.service.weather;
+
+import com.a201.countingstar.dto.weather.weatherResponseDto;
+
+import java.util.List;
+
+public interface WeatherService {
+    public List<weatherResponseDto> getWeather(String baseDateYear,
+                                               String baseDateMonth,
+                                               String baseDateDay,
+                                               String baseDateHour,
+                                               String baseDateMinute,
+                                               int spotId);
+}
