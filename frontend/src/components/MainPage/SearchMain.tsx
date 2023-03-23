b28a4d6c64f459dc3546c81799d3fa1b71b9c1d8
ljh991004 <ljh991004@naver.com>
2023-03-23 13:33:48 +0900

    💫 [S08P22A201-203] FE / 검색 컴포넌트 세부 구현

--- frontend/src/components/MainPage/SearchMain.tsx
+++ frontend/src/components/MainPage/SearchMain.tsx
@@ -13,8 +13,8 @@ interface hourToString {
 function SearchMain({}: Props) {
   //시간, 날짜, 시도, 구군
   const [timeValue, setTimeValue] = useState<string>("시간 선택");
-  const [dateValue, setDateValue] = useState<string>("지역 선택");
-  const [sidoValue, setSidoValue] = useState<string>("");
+  const [dateValue, setDateValue] = useState<string>("날짜 선택");
+  const [sidoValue, setSidoValue] = useState<string>("시도 선택");
   const [gugunValue, setGugunValue] = useState<string>("");
 
   //시간(1) option(01시)로 변경하기
@@ -32,7 +32,7 @@ function SearchMain({}: Props) {
   }
 
   let sidos: string[] = [
-    // "지역 선택",
+    "지역 선택",
     "서울특별시",
     "광주광역시",
     "대구광역시",
@@ -56,13 +56,29 @@ function SearchMain({}: Props) {
     setTimeValue(e.target.value);
   };
 
+  const handleSelectDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDateValue(e.target.value);
+  };
+
   const handleSelectSido = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSidoValue(e.target.value);
   };
 
-  const handleSearch = (e: React.MouseEvent) => {};
-  const handleSearchContent = (e: React.FormEventHandler<HTMLFormElement>) => {
-    setGugunValue(e.target.value);
+  const handleSearchButton = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(sidoValue);
+    console.log(gugunValue);
+    console.log(dateValue);
+    console.log(timeValue);
+
+    //request 보내야 하는 부분
+  };
+  const handleSearchInput = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setGugunValue(value);
   };
 
   return (
@@ -89,6 +105,8 @@ function SearchMain({}: Props) {
         min=""
         max=""
         className="w-1/6 ml-1 bg-white border border-gray-200 rounded-2xl shadow-md text-center"
+        value={dateValue}
+        onChange={handleSelectDate}
       ></input>
 
       <select
@@ -105,13 +123,13 @@ function SearchMain({}: Props) {
       </select>
 
       <div className="bg-white border border-gray-200 rounded-2xl shadow-md my-2 ">
-        <form className="grid grid-cols-12 gap-1" onSubmit={handleSearch}>
+        <form className="grid grid-cols-12 gap-1" onSubmit={handleSearchButton}>
           <input
             className="rounded-3xl col-span-11 p-15 text-center text-2xl font-serif"
             type="text"
-            value={search}
+            value={gugunValue}
             placeholder="구/군을 검색해보세요!"
-            onChange={handleSearchContent}
+            onChange={handleSearchInput}
           ></input>
           <button className="col-span-1 rounded-3xl" type="submit">
             <img src={search} className="w-3/4 h3/4 p-3" alt="searchIcon" />
