6dada08eaa50ead4d16531b27b31fb8750433b46
홍빈 <105191633+hong-bin95@users.noreply.github.com>
2023-03-24 15:00:48 +0900

    🚀 [S08P22A201-173] BE 날씨 컨디션, 미세먼지 api 추가

--- backend/src/main/java/com/a201/countingstar/dto/weather/weatherResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.a201.countingstar.dto.weather;
-
-public class weatherResponseDto {
-    // 날씨 : 맑음, 흐림, 안개 등
-    private String weatherCondition;
-    // 미세먼지 : 좋음, 나쁨, 매우나쁨 등
-    private String DustCondition;
-}
--- backend/src/main/java/com/a201/countingstar/service/weather/WeatherServiceImpl.java
+++ backend/src/main/java/com/a201/countingstar/service/weather/WeatherServiceImpl.java
@@ -1,12 +1,53 @@
 package com.a201.countingstar.service.weather;
 
-import com.a201.countingstar.dto.weather.weatherResponseDto;
+import com.a201.countingstar.dto.weather.ConditionResponseDto;
+import com.a201.countingstar.dto.weather.DustResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+
+@Service
+@RequiredArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
     @Override
-    public List<weatherResponseDto> getWeather(String baseDateYear, String baseDateMonth, String baseDateDay, String baseDateHour, String baseDateMinute, int spotId) {
+    public ConditionResponseDto getCondition(String baseDateYear, String baseDateMonth, String baseDateDay, String baseDateHour , int spotId) throws Exception {
+
+        /* 날씨 api의 경우 새벽 2시가 지나야 가능하다
+        * 기준 시간은 1시간 전 (보통 30분에 발표나기 때문)
+        *
+        * 0시간 ~ 48시간 : 단기예보
+        * 그 이상 : 중단기예보 (오전오후 나누어서)
+        * */
+
+        String date_str = String.format("%s-%s-%s %s",baseDateYear, baseDateMonth, baseDateDay, baseDateHour);
+        SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH");
+        Date date = transFormat.parse(date_str);
+
+        Date now = new Date();
+
+        return null;
+    }
+
+
+    @Override
+    public DustResponseDto getDust(String baseDateYear, String baseDateMonth, String baseDateDay, String baseDateHour , int spotId) throws Exception {
+
+        /* 날씨 api의 경우 새벽 2시가 지나야 가능하다
+         * 기준 시간은 1시간 전 (보통 30분에 발표나기 때문)
+         *
+         * 0시간 ~ 48시간 : 단기예보
+         * 그 이상 : 중단기예보 (오전오후 나누어서)
+         * */
+
+        String date_str = String.format("%s-%s-%s %s",baseDateYear, baseDateMonth, baseDateDay, baseDateHour);
+        SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH");
+        Date date = transFormat.parse(date_str);
+
+        Date now = new Date();
+
         return null;
     }
 }
