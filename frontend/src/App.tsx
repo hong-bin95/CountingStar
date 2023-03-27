ed185bd71e28df65be86f4890e41683e6958948e
Son Jeong Hun <thswjdgns9@naver.com>
2023-03-27 15:45:37 +0900

    🚀 FE / 파일 이름 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import store from './store/store';
-import Main from './pages/Main';
-import Detail from './pages/Detail';
-import GoogleMain from './pages/GoogleMain';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/Store";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import GoogleMain from "./pages/GoogleMain";
 
 function App() {
   return (
     <Provider store={store}>
       <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/detail" element={<Detail/>} />
-        <Route path="/googlemain" element={<GoogleMain/>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/googlemain" element={<GoogleMain />} />
       </Routes>
     </Provider>
   );
