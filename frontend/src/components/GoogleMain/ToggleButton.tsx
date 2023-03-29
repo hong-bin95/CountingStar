b02a1cf4f0f2475acc29a94fe596b570d7fa1f24
Son Jeong Hun <thswjdgns9@naver.com>
2023-03-29 10:14:03 +0900

    🚀 [S08P22A201-225] FE / 구글맵 - 컴포넌트 css

--- frontend/src/components/GoogleMain/ToggleButton.tsx
+++ frontend/src/components/GoogleMain/ToggleButton.tsx
@@ -5,12 +5,15 @@ const ToggleButton: React.FC<ToggleButtonType> = ({
   onClick,
   isMainVisible,
 }) => {
-  const buttonText = isMainVisible ? "Hide Main" : "Show Main";
+  const buttonText = isMainVisible
+    ? "지도에서 보기"
+    : "오늘은 어디에 별이 뜰까요?";
 
   return (
     <button
       onClick={onClick}
       className="toggle-button fixed top-4 right-6 z-50
+      w-36
       bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
     >
       {buttonText}
