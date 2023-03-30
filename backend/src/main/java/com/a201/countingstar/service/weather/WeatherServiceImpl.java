38a94b23d13f0301d303fb54b757c6c4d9a3af15
홍빈 <105191633+hong-bin95@users.noreply.github.com>
2023-03-30 11:09:39 +0900

    💫 [S08P22A201-173] BE 날씨 컨디션, 미세먼지 작업중

--- backend/src/main/java/com/a201/countingstar/service/weather/WeatherServiceImpl.java
+++ backend/src/main/java/com/a201/countingstar/service/weather/WeatherServiceImpl.java
@@ -1,17 +1,29 @@
 package com.a201.countingstar.service.weather;
 
+import com.a201.countingstar.common.CallAPI;
 import com.a201.countingstar.dto.weather.ConditionResponseDto;
 import com.a201.countingstar.dto.weather.DustResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
 @RequiredArgsConstructor
 public class WeatherServiceImpl implements WeatherService {
+
+    @Value("${cstar.weather.key}")
+    private String weatherKey;
+
+    private final CallAPI call;
+
+
     @Override
     public ConditionResponseDto getCondition(String baseDateYear, String baseDateMonth, String baseDateDay, String baseDateHour , int spotId) throws Exception {
 
@@ -24,14 +36,31 @@ public class WeatherServiceImpl implements WeatherService {
 
         String date_str = String.format("%s-%s-%s %s",baseDateYear, baseDateMonth, baseDateDay, baseDateHour);
         SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH");
+        
+        // 기준 시간
         Date date = transFormat.parse(date_str);
 
-        date = date
+        date = addHoursToJavaUtilDate(date, -1);
 
         Date now = new Date();
 
         // 현재와 기준 시간의 차이 (단위 : 시간)
         long Hour = (date.getTime() - now.getTime()) / 3600000;
+
+        if(Hour <= 48){
+            // 단기예보
+            Map<String, String> map = new HashMap<>();
+
+            map.put("Content-type","application/json");
+
+            String result = call.GET("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"
+                    ,map);
+
+            System.out.println("api 호출 결과 : " + result);
+        }
+        else{
+            // 중단기예보
+        }
 
         return null;
     }
@@ -54,5 +83,12 @@ public class WeatherServiceImpl implements WeatherService {
         Date now = new Date();
 
         return null;
+    }
+
+    public Date addHoursToJavaUtilDate(Date date, int hours) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, hours);
+        return calendar.getTime();
     }
 }
