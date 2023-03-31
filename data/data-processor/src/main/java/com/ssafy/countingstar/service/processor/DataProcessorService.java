58a41a01c7cb23813895071aa423cd01d7ecfbfe
강승권 <kangsg10@naver.com>
2023-03-31 18:16:58 +0900

    🚀[S08P22A201-188] Data-Processor 구조 변경 및 Spring 의존 추가

---
+++ data/data-processor/src/main/java/com/ssafy/countingstar/service/processor/DataProcessorService.java
@@ -0,0 +1,9 @@
+package com.ssafy.countingstar.service.processor;
+
+import java.time.LocalDateTime;
+
+public interface DataProcessorService {
+	
+	public void process(LocalDateTime dateTime);
+
+}
